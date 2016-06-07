941f56c2d2bea95b47a5a43b0cde6cd0d9d4ff26
ruby <liyufeng_23@163.com>
2016-06-07 17:52:31 +0800

    init

---
+++ src/gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var webpack = require('gulp-webpack');
+gulp.task('default', function() {
+  return gulp.src('./js')
+    .pipe(webpack( require('./webpack.config.js') ))
+    .pipe(gulp.dest('dist/'));
+});
